543038c7ceb3ce3b49d747de7e1bbe821e381fc7
Frans Skarman <frans.skarman@gmail.com>
2016-01-19 16:03:25 +0100

    Moved struct, implemented append

--- lab03/main.c
+++ lab03/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct list_item 
+{
+    int value;
+    struct list_item * next;
+};
 
 void append(struct list_item *first, int x) /* puts x at the end of the list */
 {
+    list_item* current = first;
+    
+    while(current->next != NULL)
+    {
+        current = current->next;
+    }
+
+    current->next = (list_item*) malloc(sizeof(list_item));
+
+    current->next->value = x;
 }
 
 void prepend(struct list_item *first, int x) /* puts x at the beginning of the list */
@@ -20,13 +37,9 @@ void input_sorted(struct list_item *first, int x)
 
 void destroy(struct list_item *first) /* free everything dynamically allocated */
 {
+    
+    
 }
-
-struct list_item 
-{
-    int value;
-    struct list_item * next;
-};
 
 int main()
 {
