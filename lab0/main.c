68f934e11d42ec2bd82c751589008fae0ff4378d
Frans Skarman <frans.skarman@gmail.com>
2016-01-19 15:50:33 +0100

    Fix lab0.2

--- lab0/main.c
+++ lab0/main.c
@@ -2,26 +2,30 @@
 
 int main(int argc, char ** argv)
 {
-  char str[] = "sihtgubed";
-  char *stri = &str[8];
-  char *buf[9];
-  char **bufi, **bufend;
-  bufi = buf;
-  bufend = &buf[9];
+  char str[] = "sihtgubed"; //Create weird array
+  char *stri = &str[8]; //stri points to the address of the 8th element
+  char buf[9]; //Array of char ptrs length 9
+  char *bufi, *bufend; //Pointers to pointers for incrementation
+  bufi = buf; //Set buffi to beginning
+  bufend = &buf[9]; //Set buffend outside buff
 
+  //Reverse str
   while (bufi != bufend){
-    *bufi = stri;
+    *bufi = *stri; 
     bufi++;
     stri--;
   }
 
+  
+  //Convert to upper  case
   while (bufi != buf){
-    *(*bufi) -= 32;
+    (*bufi) -= 32;
     bufi--;
   }
 
+  //Print result
   while (bufi != bufend){
-    printf("%c", **bufi);
+    printf("%c", *bufi);
     bufi++;
   }
 }
