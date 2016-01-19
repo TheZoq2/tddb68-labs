87bc2dde187dd5e5a68e88af50049879153a6ed7
Frans Skarman <frans.skarman@gmail.com>
2016-01-19 15:56:41 +0100

    Add skeleton to lab03

--- lab03/main.c
+++ lab03/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+void append(struct list_item *first, int x); /* puts x at the end of the list */
+void prepend(struct list_item *first, int x); /* puts x at the beginning of the list */
+void print(struct list_item *first);  /* prints all elements in the list */
+/* input_sorted: find the first element in the list larger than x
+   and input x right before that element */
+void input_sorted(struct list_item *first, int x);
+void destroy(struct list_item *first); /* free everything dynamically allocated */
+
+struct list_item 
+{
+    int value;
+    struct list_item * next;
+};
+
 int main()
 {
+    struct list_item root;
+    root.value = -1; /* This value is always ignored */
+    root.next = NULL;
+
     return 0;
 }
