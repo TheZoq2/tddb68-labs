281a51e5900b01262faa1295d69df8793a99da3b
Frans Skarman <frans.skarman@gmail.com>
2016-01-19 15:58:06 +0100

    add skeleton code

--- lab03/main.c
+++ lab03/main.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
-void append(struct list_item *first, int x); /* puts x at the end of the list */
-void prepend(struct list_item *first, int x); /* puts x at the beginning of the list */
-void print(struct list_item *first);  /* prints all elements in the list */
+void append(struct list_item *first, int x) /* puts x at the end of the list */
+{
+}
+
+void prepend(struct list_item *first, int x) /* puts x at the beginning of the list */
+{
+}
+
+void print(struct list_item *first) /* prints all elements in the list */
+{
+}
+
 /* input_sorted: find the first element in the list larger than x
    and input x right before that element */
-void input_sorted(struct list_item *first, int x);
-void destroy(struct list_item *first); /* free everything dynamically allocated */
+void input_sorted(struct list_item *first, int x)
+{
+}
+
+void destroy(struct list_item *first) /* free everything dynamically allocated */
+{
+}
 
 struct list_item 
 {
