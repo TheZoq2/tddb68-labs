5bde2170f8ed7a2b9b19ea2ba639b3603b0479a3
Frans Skarman <frans.skarman@gmail.com>
2016-01-29 08:56:44 +0100

    Add syscall switch statememnt

--- linux/pintos/src/userprog/syscall.c
+++ linux/pintos/src/userprog/syscall.c
@@ -18,6 +18,66 @@ syscall_handler (struct intr_frame *f UNUSED)
   printf ("system call!\n");
 
   int syscall_id = *((int*)f->esp);
+
+  switch(syscall_id) 
+  {
+    case  SYS_HALT:
+    {
+        
+      break;
+    }
+    case SYS_EXIT:
+    {
+        
+      break;
+    }
+    case SYS_EXEC:
+    {
+      break;
+    }
+    case WAIT:
+    {
+      break;
+    }
+    case SYS_CREATE:
+    {
+      break;
+    }
+    case SYS_REMOVE:
+    {
+      break;
+    }
+    case SYS_OPEN:
+    {
+      break;
+    }
+    case SYS_FILESIZE:
+    {
+      break;
+    }
+    case SYS_READ:
+    {
+      break;
+    }
+    case SYS_WRITE:
+    {
+      break;
+    }
+    case SYS_SEEK:
+    {
+      break;
+    }
+    case SYS_TELL:
+    {
+      break;
+    }
+    case SYS_CLOSE:
+    {
+      break;
+    }
+    default:
+          
+  }
   
   thread_exit ();
 }
